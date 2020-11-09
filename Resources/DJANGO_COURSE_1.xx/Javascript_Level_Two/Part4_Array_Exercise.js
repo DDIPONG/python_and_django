ac59f12ceabcb5ce132df642f8fee42da29a87fb
joon <kweonhj1542@gmail.com>
2020-11-09 21:17:46 +0900

    from work 11/09

--- Resources/DJANGO_COURSE_1.xx/Javascript_Level_Two/Part4_Array_Exercise.js
+++ Resources/DJANGO_COURSE_1.xx/Javascript_Level_Two/Part4_Array_Exercise.js
@@ -3,44 +3,43 @@
 
 // Create Empty Student Roster Array
 // This has been done for you!
-var roster = []
-var pick = prompt("what will you pick? [add] [remove] [display] [quit]")
-
-while (pick==quit){
-
-if (pick=="add") {
-    var studentName = prompt("enter Student Name")
-    addNew(studentName);
-    
-} else if(pick=="remove"{
-    var studentName = prompt("enter Student Name")
-    remove(studentName)
-    
-} else if(pick==display){
-    console.log(roaster);
+var roster = ["a","b","c"]
+function remove(){
+    for(jooooon of roster){
+        console.log(jooooon);
+        
+    }
 }
-  else if(pick==quit){
+remove();
 
-    prompt("Thank you for the service")
-  }
-
-
-}
 // Create the functions for the tasks
 
 // ADD A NEW STUDENT
 
-function addNew(name){
-    roaster.push(name)
+function addNew(){
+    var studentName = prompt("enter Student Name")
+    roaster.push(studentName)
 }
 // Create a function called addNew that takes in a name
 // and uses .push to add a new student to the array
 
 
 // REMOVE STUDENT
-function remove(name){
+// function remove(name){
+//     for(joooon of roaster){
+//         console.log(joooon);
+        
+//     }
+    for(var i=0; i<roaster(length)+1, i++;){
+        if(name==roster[i]){
+            roaster.remove(i)    
+        }
+    }
+    var studentName = prompt("enter Student Name")
     roaster.pop()
-}
+
+
+
 
 // Create a function called remove that takes in a name
 // Finds its index location, then removes that name from the roster
@@ -49,6 +48,13 @@ function remove(name){
 //
 
 // DISPLAY ROSTER
+function display(){
+    for(joooon of roaster){
+        console.log(joooon);
+        
+    }
+
+}
 
 // Create a function called display that prints out the orster to the console.
 
@@ -57,3 +63,24 @@ function remove(name){
 
 // Now create a while loop that keeps asking for an action (add,remove, display or quit)
 // Use if and else if statements to execute the correct function for each command.
+var pick = prompt("Pick One [add] [remove] [display] [quit]");
+// var pick = null;
+while (pick=="quit"){
+
+    if (pick=="add") {
+        
+        addNew();
+        
+    }else if(pick==="remove"){
+        
+        remove(studentName)
+        
+    }else if(pick==="display"){
+        console.log(roaster);
+    }else if(pick==="quit"){
+    
+        console.log("Thank you for the service")
+    }
+    
+    
+}
---
+++ Resources/DJANGO_COURSE_1.xx/Javascript_Level_Two/test.js
@@ -0,0 +1,8 @@
+var roster = ["a","b","c"]
+
+function remove(name){
+    for(joooon of roaster){
+        console.log(joooon);
+        
+    }
+}
