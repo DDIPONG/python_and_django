9cbc9c886a623058b75f1b78d96e3de90e442d24
Joon@Home_Wins <kweonhj1542@gmail.com>
2021-02-08 23:27:32 +0900

    from start

--- HTML/JavaScript/Part9-1.js
+++
@@ -1,4 +0,0 @@
-function addNum(num1, num2) {
-    console.log(num1+num2);
-    
-}
--- HTML/JavaScript/myscript.js
+++
@@ -1,9 +0,0 @@
-// for (var i = 0; i<5; i++) {
-//     console.log("Number is " + i);
-
-// }
-
-var word = "ABABAABAA"
-for(var i =0; i<word.length; i++){
-    console.log(word[i]);
-}
